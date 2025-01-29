ba6c5896a13cea2cefb67d6757ece5ba40740b49
Glaison Queiroz <glaison26.queiroz@gmail.com>
2025-01-29 16:56:18 -0300

    dev

--- lancamentos/lancamentos_novo.php
+++ lancamentos/lancamentos_novo.php
@@ -8,6 +8,33 @@ if (!isset($_SESSION['newsession'])) {
 include('../conexao.php');
 include('../links2.php');
 include_once "../lib_gop.php";
+
+// variaveis para mensagens de erro e suscessso da gravação
+$msg_gravou = "";
+$msg_erro = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $c_tipo = $_POST['tipo'];
+    $c_setor = $_POST['setor'];
+    $c_fone1 = $_POST['fone1'];
+    $c_fone2 = $_POST['fone2'];
+
+    do {
+
+        // faço a inclusão da tabela com sql
+        $c_sql = "Insert into solicitantes (nome,telefone,telefone2,setor) Value ('$c_nome', '$c_fone1', '$c_fone2', '$c_setor')";
+        $result = $conection->query($c_sql);
+        // verifico se a query foi correto
+        if (!$result) {
+            die("Erro ao Executar Sql!!" . $conection->connect_error);
+        }
+
+        $msg_gravou = "Dados Gravados com Sucesso!!";
+
+        header('location: /transporte/cadastros/solicitantes/solicitantes_lista.php');
+    } while (false);
+}
 ?>
 
 
@@ -47,7 +74,19 @@ include_once "../lib_gop.php";
         }
         ?>
         <form method="post" name="frm_solicitacao">
-
+            <div class="row mb-6">
+                <label class="col-sm-2 col-form-label">Tipo de Solicitação</label>
+                <div class="col-sm-3">
+                    <select class="form-select form-select-lg mb-3" id="tipo" name="tipo" value="<?php echo $c_tipo; ?>" required>
+                        <option></option>
+                        <option>Administrativo</option>
+                        <option>Unidade de Saúde</option>
+                        <option>Oncologia</option>
+                        <option>visita domiciliar</option>
+                    </select>
+                </div>
+            </div>
+            <br>
             <div class="row mb-6">
                 <label class="col-md-2 form-label">Data</label>
                 <div class="col-sm-2">
@@ -66,82 +105,107 @@ include_once "../lib_gop.php";
                 </div>
                 <br>
             </div>
+            <br>
 
-            <label class="col-sm-3 col-form-label">Ocorrencia </label>
-            <div class="col-sm-7">
-
-                <select onchange="verifica(value)" class="form-select form-select-lg mb-3" id="ocorrencia" name="ocorrencia" value="<?php echo $c_ocorrencia ?>" required>
-
-                    <option></option>
-                    <?php
-                    // select da tabela de ocorrencia
-                    $c_sql_setor = "SELECT ocorrencias.id, ocorrencias.descricao FROM ocorrencias ORDER BY ocorrencias.descricao";
-                    $result_setor = $conection->query($c_sql_setor);
-                    while ($c_linha = $result_setor->fetch_assoc()) {
-                        if (!empty($_SESSION['valor_ocorrencia'])) {
-                            if ($_SESSION['valor_ocorrencia'] == $c_linha['descricao'])
-                                $op = 'selected';
-                            else
-                                $op = "";
+            <div class="row mb-6">
+                <label class="col-sm-2 col-form-label">Solicitante </label>
+                <div class="col-sm-3">
+                    <select class="form-select form-select-lg mb-3" id="solicitante" name="solicitante" required>
+                        <option></option>
+                        <?php
+                        // select da tabela de solicitantes
+                        $c_sql = "SELECT solicitantes.id, solicitantes.nome FROM solicitantes ORDER BY solicitantes.nome";
+                        $result = $conection->query($c_sql);
+                        while ($c_linha = $result->fetch_assoc()) {
+                            echo "  
+                          <option>$c_linha[nome]</option>
+                        ";
                         }
-                        echo "  
-          <option $op>$c_linha[descricao]</option>
-        ";
-                    }
-                    ?>
-                </select>
-
+                        ?>
+                    </select>
+                </div>
+                <label class="col-sm-2 col-form-label">Motorista </label>
+                <div class="col-sm-3">
+                    <select class="form-select form-select-lg mb-3" id="motorista" name="motorista" required>
+                        <option></option>
+                        <?php
+                        // select da tabela de motoristas
+                        $c_sql = "SELECT motoristas.id, motoristas.nome FROM motoristas ORDER BY motoristas.nome";
+                        $result = $conection->query($c_sql);
+                        while ($c_linha = $result->fetch_assoc()) {
+                            echo "  
+                          <option>$c_linha[nome]</option>
+                        ";
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
-    </div>
 
-
-    <div class="row mb-3">
-        <label class="col-sm-3 col-form-label">Tipo de Solicitação</label>
-        <div class="col-sm-2">
-            <select class="form-select form-select-lg mb-3" id="tipo" name="tipo" value="<?php echo $c_tipo; ?>" required>
-                <option></option>
-                <option>Programada</option>
-                <option>Urgência</option>
-
-            </select>
-        </div>
-        <label class="col-sm-2 col-form-label">Setor </label>
-        <div class="col-sm-3">
-            <select class="form-select form-select-lg mb-3" id="setor" name="setor" required>
-                <option></option>
-                <?php
-                // select da tabela de setores
-                $c_sql_setor = "SELECT setores.id, setores.descricao FROM setores ORDER BY setores.descricao";
-                $result_setor = $conection->query($c_sql_setor);
-                while ($c_linha = $result_setor->fetch_assoc()) {
-                    echo "  
+            <div class="row mb-6">
+                <label class="col-sm-2 col-form-label">Veículo </label>
+                <div class="col-sm-3">
+                    <select class="form-select form-select-lg mb-3" id="veiculo" name="veiculo" required>
+                        <option></option>
+                        <?php
+                        // select da tabela de veiculos
+                        $c_sql = "SELECT veiculo.id, veiculo.descricao FROM veiculo ORDER BY veiculo.descricao";
+                        $result = $conection->query($c_sql);
+                        while ($c_linha = $result->fetch_assoc()) {
+                            echo "  
                           <option>$c_linha[descricao]</option>
                         ";
-                }
-                ?>
-            </select>
-        </div>
-
-    </div>
-    <div style="padding-top:5px;">
-
-        <div class="row mb-7">
-            <label class="col-sm-3 col-form-label">Descrição </label>
-            <div class="col-sm-7">
-                <textarea class="form-control" id="solicitacao" name="solicitacao" rows="10"><?php echo $c_solicitacao; ?></textarea>
+                        }
+                        ?>
+                    </select>
+                </div>
+                <label class="col-sm-2 col-form-label">Paciente </label>
+                <div class="col-sm-3">
+                    <select class="form-select form-select-lg mb-3" id="paciente" name="paciente" required>
+                        <option></option>
+                        <?php
+                        // select da tabela de pacientes
+                        $c_sql = "SELECT paciente.id, paciente.nome FROM paciente ORDER BY paciente.nome";
+                        $result = $conection->query($c_sql);
+                        while ($c_linha = $result->fetch_assoc()) {
+                            echo "  
+                          <option>$c_linha[nome]</option>
+                        ";
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
-        </div>
+            <br>
+            <div class="row mb-3">
+               
+                    <label class="col-sm-2 col-form-label">Justificativa</label>
+                    <div class="col-sm-8">
+                    <textarea class="form-control" id="solicitacao" name="solicitacao" rows="6"></textarea>
+                    </div>
+              
+            </div>
 
+            <hr>
+            <br>
+            <div class="row mb-3">
+                <div class="offset-sm-0 col-sm-3">
+                    <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Salvar</button>
+                    <a class='btn btn-danger' href='/transporte/lancamentos/lancamentos.php'><span class='glyphicon glyphicon-remove'></span> Cancelar</a>
+                </div>
+
+            </div>
+
+        </form>
     </div>
-    <hr>
-    <div class="row mb-3">
-        <div class="offset-sm-0 col-sm-3">
-            <button type="submit" class="btn btn-primary"><span class='glyphicon glyphicon-floppy-saved'></span> Finalizar</button>
-            <a class='btn btn' href='/gop/solicitacao/solicitacao_nova.php'><img src="\gop\images\saida.png" alt="" width="25" height="25"> Voltar</a>
-        </div>
-    </div>
+
+
+
+
+
+
     </form>
-    </div>
+
 
 
 
